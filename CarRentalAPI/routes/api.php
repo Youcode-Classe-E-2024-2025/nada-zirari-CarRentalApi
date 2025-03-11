0ea0065656fd32c236a467a135d8a5219a6c9205
Nada Zirari <zirarinada20@gmail.com>
2025-03-11 12:33:45 +0000

    install sanctum

--- CarRentalAPI/routes/api.php
+++ CarRentalAPI/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\CarController;
+use App\Http\Controllers\API\RentalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('cars', CarController::class);
+Route::apiResource('rentals', RentalController::class);
