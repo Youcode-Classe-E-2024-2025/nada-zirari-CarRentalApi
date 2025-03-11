9f5380ecd9defed74db4841355d36688737fcb92
Nada Zirari <zirarinada20@gmail.com>
2025-03-11 14:50:40 +0000

    add model concernant le payment

--- CarRentalAPI/app/Models/Payment.php
+++ CarRentalAPI/app/Models/Payment.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+
+
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'rental_id',
+        'amount',
+        'payment_method',
+        'transaction_id',
+        'status'
+    ];
+
+    public function rental()
+    {
+        return $this->belongsTo(Rental::class);
+    }
 }
